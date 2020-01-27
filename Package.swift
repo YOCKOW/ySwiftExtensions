973767cf18844eb3e4c67da47382c4211f409b1e
YOCKOW <YOCKOW@users.noreply.github.com>
2020-01-27 13:16:40 +0900

    Detach a supporting module.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,9 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(name: "yExtensions", dependencies: ["SwiftUnicodeSupplement"]),
-    .target(name: "yProtocols"),
+    .target(name: "_yExtensions_support"),
+    .target(name: "yExtensions", dependencies: ["SwiftUnicodeSupplement", "_yExtensions_support"]),
+    .target(name: "yProtocols", dependencies: ["_yExtensions_support"]),
     .testTarget(name: "yExtensionsTests", dependencies: ["yExtensions"]),
     .testTarget(name: "yProtocolsTests", dependencies: ["yProtocols"]),
   ],
