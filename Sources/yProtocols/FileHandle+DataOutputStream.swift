536a04f25b4a10155072444f829c0869c9d4cbbf
YOCKOW <YOCKOW@users.noreply.github.com>
2020-04-14 10:14:21 +0900

    Change signatures of `DataOutputStream[able]`.

--- Sources/yProtocols/FileHandle+DataOutputStream.swift
+++ Sources/yProtocols/FileHandle+DataOutputStream.swift
@@ -8,26 +8,21 @@
 import Foundation
 import yNewAPI
 
+// `func write(contentsOf:)` is available in macOS>=10.15
+// extension FileHandle: DataOutputStream {}
+
 private let _SIZE_TO_READ = 1024
-extension FileHandle: DataOutputStream, DataOutputStreamable {
-  public func write<D>(_ data: D) where D: DataProtocol {
-    try? self.newAPI.write(contentsOf: data)
-  }
-  
-  public func write<Target>(to target: inout Target) where Target: DataOutputStream {
-    let originalOffset = try? self.newAPI.offset()
-    defer {
-      if let offset = originalOffset {
-        try? self.newAPI.seek(toOffset: offset)
-      }
-    }
+extension FileHandle: DataOutputStreamable {
+  public func write<Target>(to target: inout Target) throws where Target : DataOutputStream {
+    let originalOffset = try self.newAPI.offset()
     
     while true {
       // "try?" causes double-optional value in Swift <5.0
       var nilableData: Data? = nil
       do { nilableData = try self.newAPI.read(upToCount: _SIZE_TO_READ) } catch {}
       guard let data = nilableData else { break }
-      target.write(data)
+      try target.write(contentsOf: data)
     }
+    try self.newAPI.seek(toOffset: originalOffset)
   }
 }
--- Tests/yProtocolsTests/DataOutputStreamTests.swift
+++ Tests/yProtocolsTests/DataOutputStreamTests.swift
@@ -11,29 +11,21 @@ import Foundation
 import _yExtensionsTests_support
 
 final class DataOutputStreamTests: XCTestCase {
-  func test_data() {
+  func test_data() throws {
     let source = Data([0,1,2,3])
     var target = Data()
     
-    source.write(to: &target)
+    try source.write(to: &target)
     XCTAssertEqual(source, target)
   }
   
   func test_fileHandle() throws {
     let data = Data([0,1,2,3])
     let source = try _temporaryFileHandle(contents: data)
-    var target = try _temporaryFileHandle()
+    var target = AnyFileHandle(try _temporaryFileHandle())
     
-    source.write(to: &target)
-    if #available(macOS 10.15, *) {
-      #if canImport(ObjectiveC) || swift(>=5.0)
-      try target.seek(toOffset: 0)
-      #else
-      target.seek(toFileOffset: 0)
-      #endif
-    } else {
-      target.seek(toFileOffset: 0)
-    }
+    try source.write(to: &target)
+    try target.seek(toOffset: 0)
     XCTAssertEqual(data, target.availableData)
   }
 }
