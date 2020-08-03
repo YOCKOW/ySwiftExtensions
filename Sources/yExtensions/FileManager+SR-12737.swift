031ab97d727e4b549243439967af6571cee762e8
YOCKOW <YOCKOW@users.noreply.github.com>
2020-08-03 17:02:31 +0900

    Fix a bug of the workaround for SR-12737.

--- Sources/yExtensions/FileManager+SR-12737.swift
+++ Sources/yExtensions/FileManager+SR-12737.swift
@@ -7,26 +7,32 @@
  
 import Foundation
 
+private let _directoryCreationQue = DispatchQueue(
+  label: "jp.YOCKOW.ySwiftExtensions.FileManager.directoryCreation",
+  qos: .background
+)
+
 extension FileManager {
   /// Workaround for [SR-12737](https://bugs.swift.org/browse/SR-12737).
   /// Creates a directory with the given attributes at the specified URL,
   /// creating also intermediate directories.
   public func createDirectoryWithIntermediateDirectories(at url: URL,
                                                          attributes: [FileAttributeKey: Any]? = nil) throws {
-    
-    #if !canImport(ObjectiveC)
-    func __createParentDirectory(of url: URL) throws {
-      try self.createDirectoryWithIntermediateDirectories(
-        at: url.standardizedFileURL.deletingLastPathComponent(), attributes: attributes
-      )
+    func __create(at url: URL) throws {
+      #if !canImport(ObjectiveC) && compiler(<5.3)
+      if url.isExistingLocalDirectory {
+        return
+      }
+      let parent = url.standardizedFileURL.deletingLastPathComponent()
+      if !parent.isExistingLocalDirectory {
+        try __create(at: parent)
+      }
+      #endif
+      try self.createDirectory(at: url, withIntermediateDirectories: true, attributes: attributes)
     }
     
-    if url.isExistingLocalDirectory {
-      return
-    } else {
-      try __createParentDirectory(of: url)
+    try _directoryCreationQue.sync {
+      try __create(at: url)
     }
-    #endif
-    try self.createDirectory(at: url, withIntermediateDirectories: true, attributes: attributes)
   }
 }
