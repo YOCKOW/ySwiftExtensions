1e6c08bc25756a1e54214bb72ea8d02271fdc0a4
YOCKOW <YOCKOW@users.noreply.github.com>
2021-01-11 11:06:16 +0900

    Move `func compareCount(with:)` from `StringProtocol` to `Collection`.

--- Tests/yExtensionsTests/CollectionCountTests.swift
+++ Tests/yExtensionsTests/CollectionCountTests.swift
@@ -1,5 +1,5 @@
 /* *************************************************************************************************
- StringProtocolCountTests.swift
+ CollectionCountTests.swift
    © 2020 YOCKOW.
      Licensed under MIT License.
      See "LICENSE.txt" for more information.
@@ -9,8 +9,8 @@ import Foundation
 import XCTest
 @testable import yExtensions
 
-final class StringProtocolCountTests: XCTestCase {
-  func test_countComparison() {
+final class CollectionCountTests: XCTestCase {
+  func test_string() {
     let emptyString = ""
     XCTAssertEqual(emptyString.compareCount(with: -1), .orderedDescending)
     XCTAssertEqual(emptyString.compareCount(with: 0), .orderedSame)
@@ -20,5 +20,17 @@ final class StringProtocolCountTests: XCTestCase {
     XCTAssertEqual(yockowString.compareCount(with: 3), .orderedDescending)
     XCTAssertEqual(yockowString.compareCount(with: 6), .orderedSame)
     XCTAssertEqual(yockowString.compareCount(with: 9), .orderedAscending)
+  }
+
+  func test_array() {
+    let emptyArray = Array<Int>()
+    XCTAssertEqual(emptyArray.compareCount(with: -1), .orderedDescending)
+    XCTAssertEqual(emptyArray.compareCount(with: 0), .orderedSame)
+    XCTAssertEqual(emptyArray.compareCount(with: 1), .orderedAscending)
+
+    let array = [0, 1, 2, 3, 4, 5]
+    XCTAssertEqual(array.compareCount(with: 3), .orderedDescending)
+    XCTAssertEqual(array.compareCount(with: 6), .orderedSame)
+    XCTAssertEqual(array.compareCount(with: 9), .orderedAscending)
   }
 }
